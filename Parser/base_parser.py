6a65bd04093871c583a7d211f8ac34084036b94b
MrMegnis <mr.megnis@mail.ru>
2023-08-22 22:47:20 +0500

    create base parser

---
+++ Parser/base_parser.py
@@ -0,0 +1,20 @@
+class BaseParser:
+    def __init__(self):
+        pass
+
+    def parse_picture(self):
+        pass
+
+    def parse_popular(self):
+        pass
+
+    def parse_tag(self):
+        pass
+
+
+def main():
+    print("Hello world")
+
+
+if __name__ == "__main__":
+    main()
