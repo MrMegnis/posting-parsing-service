f1854481f86831b885cf6f348fae0b3b262503b0
MrMegnis <97959994+MrMegnis@users.noreply.github.com>
2023-08-27 17:02:52 +0500

    Update test2.py

--- Parser/test2.py
+++ Parser/test2.py
@@ -1,11 +1,93 @@
-{'illust': {'id': 111035834, 'title': '', 'type': 'illust',
-            'image_urls': {'square_medium': 'https://s.pximg.net/common/images/limit_sanity_level_360.png',
-                           'medium': 'https://s.pximg.net/common/images/limit_sanity_level_360.png',
-                           'large': 'https://s.pximg.net/common/images/limit_sanity_level_360.png'}, 'caption': '',
-            'restrict': 0, 'user': {'id': 91718035, 'name': '', 'account': '', 'profile_image_urls': {
-        'medium': 'https://s.pximg.net/common/images/no_profile.png'}}, 'tags': [], 'tools': [],
-            'create_date': '2023-08-27T19:56:28+09:00', 'page_count': 1, 'width': 100, 'height': 100, 'sanity_level': 4,
-            'x_restrict': 0, 'series': None,
-            'meta_single_page': {'original_image_url': 'https://s.pximg.net/common/images/limit_sanity_level_360.png'},
-            'meta_pages': [], 'total_view': 0, 'total_bookmarks': 0, 'is_bookmarked': False, 'visible': False,
-            'is_muted': False, 'illust_ai_type': 0, 'illust_book_style': 0, 'comment_access_control': 0}}
+{'illust': {'id': 111162154, 'title': 'Yae Miko, lumine,Raiden Shogun', 'type': 'illust', 'image_urls': {
+    'square_medium': 'https://i.pximg.net/c/540x540_10_webp/img-master/img/2023/08/26/03/50/55/111162154_p0_square1200.jpg',
+    'medium': 'https://i.pximg.net/c/540x540_70/img-master/img/2023/08/26/03/50/55/111162154_p0_master1200.jpg',
+    'large': 'https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/08/26/03/50/55/111162154_p0_master1200.jpg'},
+            'caption': '', 'restrict': 0, 'user': {'id': 94666804, 'name': 'Sperm123', 'account': 'user_fhgz7284',
+                                                   'profile_image_urls': {
+                                                       'medium': 'https://i.pximg.net/user-profile/img/2023/05/27/23/04/51/24473039_bc69028eeeb01181c7070628d4635e69_170.jpg'},
+                                                   'is_followed': False},
+            'tags': [{'name': 'R-18', 'translated_name': None}, {'name': 'AI', 'translated_name': None},
+                     {'name': 'AIイラスト', 'translated_name': None}, {'name': 'waifu', 'translated_name': None},
+                     {'name': 'nsfw', 'translated_name': None}, {'name': 'GenshinImpact', 'translated_name': None},
+                     {'name': 'lumine', 'translated_name': None}, {'name': 'yaemiko', 'translated_name': None},
+                     {'name': 'raidenshogun', 'translated_name': None}], 'tools': [],
+            'create_date': '2023-08-26T03:50:55+09:00', 'page_count': 19, 'width': 2828, 'height': 2828,
+            'sanity_level': 6, 'x_restrict': 1, 'series': None, 'meta_single_page': {}, 'meta_pages': [{'image_urls': {
+        'square_medium': 'https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/08/26/03/50/55/111162154_p0_square1200.jpg',
+        'medium': 'https://i.pximg.net/c/540x540_70/img-master/img/2023/08/26/03/50/55/111162154_p0_master1200.jpg',
+        'large': 'https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/08/26/03/50/55/111162154_p0_master1200.jpg',
+        'original': 'https://i.pximg.net/img-original/img/2023/08/26/03/50/55/111162154_p0.jpg'}}, {'image_urls': {
+        'square_medium': 'https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/08/26/03/50/55/111162154_p1_square1200.jpg',
+        'medium': 'https://i.pximg.net/c/540x540_70/img-master/img/2023/08/26/03/50/55/111162154_p1_master1200.jpg',
+        'large': 'https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/08/26/03/50/55/111162154_p1_master1200.jpg',
+        'original': 'https://i.pximg.net/img-original/img/2023/08/26/03/50/55/111162154_p1.jpg'}}, {'image_urls': {
+        'square_medium': 'https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/08/26/03/50/55/111162154_p2_square1200.jpg',
+        'medium': 'https://i.pximg.net/c/540x540_70/img-master/img/2023/08/26/03/50/55/111162154_p2_master1200.jpg',
+        'large': 'https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/08/26/03/50/55/111162154_p2_master1200.jpg',
+        'original': 'https://i.pximg.net/img-original/img/2023/08/26/03/50/55/111162154_p2.jpg'}}, {'image_urls': {
+        'square_medium': 'https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/08/26/03/50/55/111162154_p3_square1200.jpg',
+        'medium': 'https://i.pximg.net/c/540x540_70/img-master/img/2023/08/26/03/50/55/111162154_p3_master1200.jpg',
+        'large': 'https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/08/26/03/50/55/111162154_p3_master1200.jpg',
+        'original': 'https://i.pximg.net/img-original/img/2023/08/26/03/50/55/111162154_p3.jpg'}}, {'image_urls': {
+        'square_medium': 'https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/08/26/03/50/55/111162154_p4_square1200.jpg',
+        'medium': 'https://i.pximg.net/c/540x540_70/img-master/img/2023/08/26/03/50/55/111162154_p4_master1200.jpg',
+        'large': 'https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/08/26/03/50/55/111162154_p4_master1200.jpg',
+        'original': 'https://i.pximg.net/img-original/img/2023/08/26/03/50/55/111162154_p4.jpg'}}, {'image_urls': {
+        'square_medium': 'https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/08/26/03/50/55/111162154_p5_square1200.jpg',
+        'medium': 'https://i.pximg.net/c/540x540_70/img-master/img/2023/08/26/03/50/55/111162154_p5_master1200.jpg',
+        'large': 'https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/08/26/03/50/55/111162154_p5_master1200.jpg',
+        'original': 'https://i.pximg.net/img-original/img/2023/08/26/03/50/55/111162154_p5.jpg'}}, {'image_urls': {
+        'square_medium': 'https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/08/26/03/50/55/111162154_p6_square1200.jpg',
+        'medium': 'https://i.pximg.net/c/540x540_70/img-master/img/2023/08/26/03/50/55/111162154_p6_master1200.jpg',
+        'large': 'https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/08/26/03/50/55/111162154_p6_master1200.jpg',
+        'original': 'https://i.pximg.net/img-original/img/2023/08/26/03/50/55/111162154_p6.jpg'}}, {'image_urls': {
+        'square_medium': 'https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/08/26/03/50/55/111162154_p7_square1200.jpg',
+        'medium': 'https://i.pximg.net/c/540x540_70/img-master/img/2023/08/26/03/50/55/111162154_p7_master1200.jpg',
+        'large': 'https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/08/26/03/50/55/111162154_p7_master1200.jpg',
+        'original': 'https://i.pximg.net/img-original/img/2023/08/26/03/50/55/111162154_p7.jpg'}}, {'image_urls': {
+        'square_medium': 'https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/08/26/03/50/55/111162154_p8_square1200.jpg',
+        'medium': 'https://i.pximg.net/c/540x540_70/img-master/img/2023/08/26/03/50/55/111162154_p8_master1200.jpg',
+        'large': 'https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/08/26/03/50/55/111162154_p8_master1200.jpg',
+        'original': 'https://i.pximg.net/img-original/img/2023/08/26/03/50/55/111162154_p8.jpg'}}, {'image_urls': {
+        'square_medium': 'https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/08/26/03/50/55/111162154_p9_square1200.jpg',
+        'medium': 'https://i.pximg.net/c/540x540_70/img-master/img/2023/08/26/03/50/55/111162154_p9_master1200.jpg',
+        'large': 'https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/08/26/03/50/55/111162154_p9_master1200.jpg',
+        'original': 'https://i.pximg.net/img-original/img/2023/08/26/03/50/55/111162154_p9.jpg'}}, {'image_urls': {
+        'square_medium': 'https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/08/26/03/50/55/111162154_p10_square1200.jpg',
+        'medium': 'https://i.pximg.net/c/540x540_70/img-master/img/2023/08/26/03/50/55/111162154_p10_master1200.jpg',
+        'large': 'https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/08/26/03/50/55/111162154_p10_master1200.jpg',
+        'original': 'https://i.pximg.net/img-original/img/2023/08/26/03/50/55/111162154_p10.jpg'}}, {'image_urls': {
+        'square_medium': 'https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/08/26/03/50/55/111162154_p11_square1200.jpg',
+        'medium': 'https://i.pximg.net/c/540x540_70/img-master/img/2023/08/26/03/50/55/111162154_p11_master1200.jpg',
+        'large': 'https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/08/26/03/50/55/111162154_p11_master1200.jpg',
+        'original': 'https://i.pximg.net/img-original/img/2023/08/26/03/50/55/111162154_p11.jpg'}}, {'image_urls': {
+        'square_medium': 'https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/08/26/03/50/55/111162154_p12_square1200.jpg',
+        'medium': 'https://i.pximg.net/c/540x540_70/img-master/img/2023/08/26/03/50/55/111162154_p12_master1200.jpg',
+        'large': 'https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/08/26/03/50/55/111162154_p12_master1200.jpg',
+        'original': 'https://i.pximg.net/img-original/img/2023/08/26/03/50/55/111162154_p12.jpg'}}, {'image_urls': {
+        'square_medium': 'https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/08/26/03/50/55/111162154_p13_square1200.jpg',
+        'medium': 'https://i.pximg.net/c/540x540_70/img-master/img/2023/08/26/03/50/55/111162154_p13_master1200.jpg',
+        'large': 'https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/08/26/03/50/55/111162154_p13_master1200.jpg',
+        'original': 'https://i.pximg.net/img-original/img/2023/08/26/03/50/55/111162154_p13.jpg'}}, {'image_urls': {
+        'square_medium': 'https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/08/26/03/50/55/111162154_p14_square1200.jpg',
+        'medium': 'https://i.pximg.net/c/540x540_70/img-master/img/2023/08/26/03/50/55/111162154_p14_master1200.jpg',
+        'large': 'https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/08/26/03/50/55/111162154_p14_master1200.jpg',
+        'original': 'https://i.pximg.net/img-original/img/2023/08/26/03/50/55/111162154_p14.jpg'}}, {'image_urls': {
+        'square_medium': 'https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/08/26/03/50/55/111162154_p15_square1200.jpg',
+        'medium': 'https://i.pximg.net/c/540x540_70/img-master/img/2023/08/26/03/50/55/111162154_p15_master1200.jpg',
+        'large': 'https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/08/26/03/50/55/111162154_p15_master1200.jpg',
+        'original': 'https://i.pximg.net/img-original/img/2023/08/26/03/50/55/111162154_p15.jpg'}}, {'image_urls': {
+        'square_medium': 'https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/08/26/03/50/55/111162154_p16_square1200.jpg',
+        'medium': 'https://i.pximg.net/c/540x540_70/img-master/img/2023/08/26/03/50/55/111162154_p16_master1200.jpg',
+        'large': 'https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/08/26/03/50/55/111162154_p16_master1200.jpg',
+        'original': 'https://i.pximg.net/img-original/img/2023/08/26/03/50/55/111162154_p16.jpg'}}, {'image_urls': {
+        'square_medium': 'https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/08/26/03/50/55/111162154_p17_square1200.jpg',
+        'medium': 'https://i.pximg.net/c/540x540_70/img-master/img/2023/08/26/03/50/55/111162154_p17_master1200.jpg',
+        'large': 'https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/08/26/03/50/55/111162154_p17_master1200.jpg',
+        'original': 'https://i.pximg.net/img-original/img/2023/08/26/03/50/55/111162154_p17.jpg'}}, {'image_urls': {
+        'square_medium': 'https://i.pximg.net/c/360x360_10_webp/img-master/img/2023/08/26/03/50/55/111162154_p18_square1200.jpg',
+        'medium': 'https://i.pximg.net/c/540x540_70/img-master/img/2023/08/26/03/50/55/111162154_p18_master1200.jpg',
+        'large': 'https://i.pximg.net/c/600x1200_90_webp/img-master/img/2023/08/26/03/50/55/111162154_p18_master1200.jpg',
+        'original': 'https://i.pximg.net/img-original/img/2023/08/26/03/50/55/111162154_p18.jpg'}}], 'total_view': 298,
+            'total_bookmarks': 49, 'is_bookmarked': False, 'visible': True, 'is_muted': False, 'total_comments': 0,
+            'illust_ai_type': 2, 'illust_book_style': 0, 'comment_access_control': 0}}
